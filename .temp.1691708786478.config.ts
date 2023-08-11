b772519afd17557332ac85462354ec04fadd588f
Alec Reimel <alecreimel1@gmail.com>
2023-08-11 19:36:08 -0400

    initial QR code component set up

--- .temp.1691708786478.config.ts
+++ .temp.1691708786478.config.ts
@@ -19,7 +19,7 @@ import { theme } from './syntax-highlighting-theme';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://grovery-core-toolkit-staging.netlify.app',
+	site: 'grovery-core-toolkit-staging.netlify.app',
 	integrations: [
 		AutoImport({
 			imports: [asideAutoImport, codeSnippetAutoImport],
