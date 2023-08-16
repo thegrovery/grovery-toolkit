5ae33f7019b3c309f47117eea650a70aef455f8d
Alec Reimel <alecreimel1@gmail.com>
2023-08-16 18:47:46 -0400

    offline page set up

--- astro.config.ts
+++ astro.config.ts
@@ -59,12 +59,12 @@ export default defineConfig({
 		        ],
 		      },
 		      workbox: {
-		        navigateFallback: '/404',
+		        navigateFallback: '/offline',
 		        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
 		      },
 		      devOptions: {
 		        enabled: true,
-		        navigateFallbackAllowlist: [/^\/404$/],
+		        navigateFallbackAllowlist: [/^\/offline/],
 		      },
 		    }),
 	],
