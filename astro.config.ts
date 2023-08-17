7bf4083a31b0aa18bb37395718313ff7c5156397
Alec Reimel <alecreimel1@gmail.com>
2023-08-16 20:23:40 -0400

    astro config rollback - trying to get PWA seetings working again

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,13 @@
 import mdx from '@astrojs/mdx';
 import preact from '@astrojs/preact';
 import { defineConfig } from 'astro/config';
-import AstroPWA from '@vite-pwa/astro';
+import AstroPWA from '@vite-pwa/astro'
+
 import AutoImport from 'astro-auto-import';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeSlug from 'rehype-slug';
 import remarkSmartypants from 'remark-smartypants';
+
 import { asideAutoImport, astroAsides } from './integrations/astro-asides';
 import { astroCodeSnippets, codeSnippetAutoImport } from './integrations/astro-code-snippets';
 import { sitemap } from './integrations/sitemap';
@@ -15,80 +17,78 @@ import { rehypeTasklistEnhancer } from './plugins/rehype-tasklist-enhancer';
 import { remarkFallbackLang } from './plugins/remark-fallback-lang';
 import { theme } from './syntax-highlighting-theme';
 
-import prefetch from "@astrojs/prefetch";
-
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://grovery-core-toolkit-staging.netlify.app/',
-  integrations: [AutoImport({
-    imports: [asideAutoImport, codeSnippetAutoImport]
-  }), preact({
-    compat: true
-  }), 
-  sitemap(), 
-  astroAsides(), 
-  astroCodeSnippets(), 
-  mdx(), 
-  AstroPWA({
-        mode: 'development',
-        base: '/en/',
-        scope: '/en/',
-        includeAssets: ['favicon.svg'],
-        registerType: 'autoUpdate',
-        manifest: {
-          name: 'Grovery CORE RMS',
-          short_name: 'CORE',
-          theme_color: '#ffffff',
-          icons: [
-            {
-              src: 'pwa-192x192.png',
-              sizes: '192x192',
-              type: 'image/png',
-            },
-            {
-              src: 'pwa-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-            },
-            {
-              src: 'pwa-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-              purpose: 'any maskable',
-            },
-          ],
-        },
-        workbox: {
-          navigateFallback: '/offline',
-          globDirectory: 'dist',
-  		globPatterns: [
-  			'**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}',
-  		],
-        },
-        devOptions: {
-          enabled: true,
-          navigateFallbackAllowlist: [/^\/offline/],
-        },
-      }),
-  prefetch()],
-  markdown: {
-    syntaxHighlight: 'shiki',
-    shikiConfig: {
-      theme
-    },
-    // Override with our own config
-    smartypants: false,
-    remarkPlugins: [[remarkSmartypants, {
-      dashes: false
-    }],
-    // Add our custom plugin that marks links to fallback language pages
-    remarkFallbackLang()],
-    rehypePlugins: [rehypeSlug,
-    // This adds links to headings
-    [rehypeAutolinkHeadings, autolinkConfig],
-    // Tweak GFM task list syntax
-    rehypeTasklistEnhancer(),
-    // Translates the autolink headings anchors
-    rehypei18nAutolinkHeadings()]
-  }
+	site: 'https://grovery-core-toolkit-staging.netlify.app/',
+	integrations: [
+		AutoImport({
+			imports: [asideAutoImport, codeSnippetAutoImport],
+		}),
+		preact({ compat: true }),
+		sitemap(),
+		astroAsides(),
+		astroCodeSnippets(),
+		mdx(),
+		AstroPWA({
+		      mode: 'development',
+		      base: '/',
+		      scope: '/',
+		      includeAssets: ['favicon.svg'],
+		      registerType: 'autoUpdate',
+		      manifest: {
+		        name: 'Grovery CORE RMS',
+		        short_name: 'CORE',
+		        theme_color: '#ffffff',
+		        icons: [
+		          {
+		            src: 'pwa-192x192.png',
+		            sizes: '192x192',
+		            type: 'image/png',
+		          },
+		          {
+		            src: 'pwa-512x512.png',
+		            sizes: '512x512',
+		            type: 'image/png',
+		          },
+		          {
+		            src: 'pwa-512x512.png',
+		            sizes: '512x512',
+		            type: 'image/png',
+		            purpose: 'any maskable',
+		          },
+		        ],
+		      },
+		      workbox: {
+		        navigateFallback: '/offline',
+		        globDirectory: '/',
+				globPatterns: [
+				'**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}',
+				],
+		      },
+		      devOptions: {
+		        enabled: true,
+		        navigateFallbackAllowlist: [/^\/offline/],
+		      },
+		    }),
+	],
+	markdown: {
+		syntaxHighlight: 'shiki',
+		shikiConfig: { theme },
+		// Override with our own config
+		smartypants: false,
+		remarkPlugins: [
+			[remarkSmartypants, { dashes: false }],
+			// Add our custom plugin that marks links to fallback language pages
+			remarkFallbackLang(),
+		],
+		rehypePlugins: [
+			rehypeSlug,
+			// This adds links to headings
+			[rehypeAutolinkHeadings, autolinkConfig],
+			// Tweak GFM task list syntax
+			rehypeTasklistEnhancer(),
+			// Translates the autolink headings anchors
+			rehypei18nAutolinkHeadings(),
+		],
+	},
 });
