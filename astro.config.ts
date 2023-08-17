2b48408be98b3b472cefe1d70cf61d59aecff451
Alec Reimel <alecreimel1@gmail.com>
2023-08-16 20:13:56 -0400

    merged astro config settings

--- astro.config.ts
+++ astro.config.ts
@@ -24,40 +24,53 @@ export default defineConfig({
     imports: [asideAutoImport, codeSnippetAutoImport]
   }), preact({
     compat: true
-  }), sitemap(), astroAsides(), astroCodeSnippets(), mdx(), AstroPWA({
-    mode: 'development',
-    base: '/',
-    scope: '/',
-    includeAssets: ['favicon.svg'],
-    registerType: 'autoUpdate',
-    manifest: {
-      name: 'Grovery CORE RMS',
-      short_name: 'CORE',
-      theme_color: '#ffffff',
-      icons: [{
-        src: 'pwa-192x192.png',
-        sizes: '192x192',
-        type: 'image/png'
-      }, {
-        src: 'pwa-512x512.png',
-        sizes: '512x512',
-        type: 'image/png'
-      }, {
-        src: 'pwa-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'any maskable'
-      }]
-    },
-    workbox: {
-      navigateFallback: '/404',
-      globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}']
-    },
-    devOptions: {
-      enabled: true,
-      navigateFallbackAllowlist: [/^\/404$/]
-    }
-  }), prefetch()],
+  }), 
+  sitemap(), 
+  astroAsides(), 
+  astroCodeSnippets(), 
+  mdx(), 
+  AstroPWA({
+        mode: 'development',
+        base: '/en/',
+        scope: '/en/',
+        includeAssets: ['favicon.svg'],
+        registerType: 'autoUpdate',
+        manifest: {
+          name: 'Grovery CORE RMS',
+          short_name: 'CORE',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
+        },
+        workbox: {
+          navigateFallback: '/offline',
+          globDirectory: 'dist',
+  		globPatterns: [
+  			'**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}',
+  		],
+        },
+        devOptions: {
+          enabled: true,
+          navigateFallbackAllowlist: [/^\/offline/],
+        },
+      }),
+  prefetch()],
   markdown: {
     syntaxHighlight: 'shiki',
     shikiConfig: {
